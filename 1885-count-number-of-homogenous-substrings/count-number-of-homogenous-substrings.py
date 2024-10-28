45ee8290da102d1c67b0a7312119cac022c4ba48
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-10-28 12:05:20 -0400

    Time: 35 ms (96.77%) | Memory: 17.2 MB (82.22%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -7,11 +7,12 @@ class Solution:
         for c in s:
             if last_char==c:
                 inc+=1
-                result+=inc
             else:
+                result+=((inc*(inc+1))//2)
                 inc=1
-                result+=1
                 last_char=c
+        
+        result+=((inc*(inc+1))//2)
         return int(result%(10e8+7))
             
 
