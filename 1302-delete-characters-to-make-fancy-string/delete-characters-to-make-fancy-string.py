ac3cf0a143671a9c6e390223b32303fe16232563
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-01 01:12:06 -0400

    Time: 191 ms (90.99%) | Memory: 18.6 MB (34.66%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -3,12 +3,14 @@ class Solution:
         if len(s)<=1:
             return s 
         result=[]
-        last_count=0
-        for c in s:
-            if len(result)==0 or result[-1]==c :
+        result.append(s[0])
+        last_count=1
+        for c in s[1:]:
+            if result[-1]==c :
                 last_count+=1
+                if last_count<3:
+                    result.append(c)
             else:
                 last_count=1
-            if last_count<3:
                 result.append(c)
         return ''.join(result)
