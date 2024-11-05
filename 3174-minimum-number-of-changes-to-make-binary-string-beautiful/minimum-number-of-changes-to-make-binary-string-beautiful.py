91a420d219ceea6a6f13e68497f0f3e9768ab82f
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-05 00:45:40 -0500

    Time: 19 ms (77.03%) | Memory: 17 MB (74.80%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,14 +1,21 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        @cache
-        def breakandbuild(string):
-            if len(string)==2:
-                return 0 if string[0]==string[1] else 1
-            div=len(string)//2
-            if div%2==0:
-                return breakandbuild(string[:div])+breakandbuild(string[div:])
-            else:
-                return breakandbuild(string[:div+1])+breakandbuild(string[div+1:])
-        result=breakandbuild(s)
-        return result    
+        changes=0
+
+        for i in range(0,len(s),2):
+            if s[i]!=s[i+1]:
+                changes+=1 
+        return changes
+
+        # @cache
+        # def breakandbuild(string):
+        #     if len(string)==2:
+        #         return 0 if string[0]==string[1] else 1
+        #     div=len(string)//2
+        #     if div%2==0:
+        #         return breakandbuild(string[:div])+breakandbuild(string[div:])
+        #     else:
+        #         return breakandbuild(string[:div+1])+breakandbuild(string[div+1:])
+        # result=breakandbuild(s)
+        # return result    
         
