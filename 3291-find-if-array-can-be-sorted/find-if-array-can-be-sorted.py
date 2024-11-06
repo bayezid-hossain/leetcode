3125e4c0bc2d10c450785694c317f48863bb4e85
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-06 00:09:08 -0500

    Time: 52 ms (23.64%) | Memory: 16.7 MB (23.35%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,17 +1,25 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-    
-        def countSetBits(num)->int:
-            total=0
-            while num:
-                total+=num&1
-                num>>=1
-            return total
-        for _ in range(len(nums)):
-            for i in range(len(nums)-1):
-                if countSetBits(nums[i])==countSetBits(nums[i+1]) and nums[i+1]<nums[i]:
-                    nums[i],nums[i+1]=nums[i+1],nums[i]
-        
-        return nums==sorted(nums)
+        length = len(nums)
+
+        # Check all pairs of elements to see if the array can be sorted
+        for i in range(length):
+            for j in range(i + 1, length):
+                # If elements are out of order
+                if nums[j] < nums[i]:
+                    # Check if they have the same bit count
+                    if bin(nums[i]).count('1') != bin(nums[j]).count('1'):
+                        # Return false if they can't be swapped to sort the array
+                        return False
+
+                    # Swap elements to move the smaller one forward
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        # Final check to ensure the array is sorted
+        for k in range(length - 1):
+            if nums[k] > nums[k + 1]:
+                return False
+
+        return True
             
             
