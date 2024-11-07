1bbb7f7b2cd51104576cd14a88ff5902dea23aff
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-06 23:53:09 -0500

    Time: 631 ms (22.50%) | Memory: 26.8 MB (81.82%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,13 +1,13 @@
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
-        mask=1
+        arr=[0]*25
         max=0
-        for shift in range(24):
-            count=0
-            for num in candidates:
-                if num & mask:
-                    count+=1
-            if count>max:
-                max=count
-            mask<<=1
+        for i in candidates:
+            br=bin(i)[2:].zfill(24)
+            # print(br)
+
+            for j in range(len(br)-1,-1,-1):
+                if br[j]=="1":
+                    arr[j]+=1
+                    max=arr[j] if arr[j]>max else max
         return max
