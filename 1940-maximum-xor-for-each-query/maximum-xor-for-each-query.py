8b7e9aaab72bb2db763d83617724feeb7d73348b
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-08 11:58:30 -0500

    Time: 44 ms (76.79%) | Memory: 32.1 MB (92.44%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -1,14 +1,16 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        maxPossible=pow(2,maximumBit)-1
-        prexor=[nums[0]]
-        n=len(nums)
-        result=[0] *n
-        result[n-1]=prexor[0]^maxPossible
-        for i in range(1,len(nums),1):
-            prexor.append(prexor[i-1]^nums[i])
-            result[n-i-1]=maxPossible^prexor[i]
-        return result
-
-
+        n = len(nums)
+        xorr = nums[0]
+        max_xor = (1 << maximumBit) - 1
+        
+        for i in range(1, n):
+            xorr ^= nums[i]
+        
+        ans = []
+        for i in range(n):
+            ans.append(xorr ^ max_xor)
+            xorr ^= nums[n - 1 - i]
+        
+        return ans
         
