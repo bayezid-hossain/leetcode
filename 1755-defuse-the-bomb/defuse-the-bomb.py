871a7e05be6330df7d01c2eb5610c11fdf7c7a8b
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-18 10:34:18 +0600

    Time: 0 ms (100.00%) | Memory: 16.7 MB (62.03%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,13 +1,15 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         
-        if k < 0: return self.decrypt(code[::-1], -k)[::-1]
-        n = len(code)
-        prefix = code * 2
-        for i in range(1, 2 * n):
-            prefix[i] += prefix[i - 1]
+        if k<0:
+            return self.decrypt(code[::-1],-k)[::-1]
+        n=len(code)
+        prefix=code*2
+        for i in range(1,2*n):
+            prefix[i]=prefix[i]+prefix[i-1]
+        
         for i in range(n):
-            code[i] = prefix[i + k] - prefix[i]
+            code[i]=prefix[i+k]-prefix[i]
         return code
     
     #[5,7,1,4]
