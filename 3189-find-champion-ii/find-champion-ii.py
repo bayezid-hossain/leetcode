db78645bef4966f2623cdb63f274fe7b8b732bd4
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-26 10:33:12 +0600

    Time: 3 ms (94.04%) | Memory: 17.7 MB (58.30%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.py
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        if n==1 and len(edges)==0:
-            return 0
-        champ_map=set({})
-        disqualified_map=set({})
-        for edge in edges:
-            if edge[0] not in disqualified_map:
-                champ_map.add(edge[0])
-            
-            if edge[1] in champ_map:
-                champ_map.remove(edge[1])
-            disqualified_map.add(edge[1])
-        if len(champ_map)+len(disqualified_map)!=n or len(champ_map)!=1:
-            return -1
-        return champ_map.pop()
+        possible_winner=[True]*n
+
+        for a,b in edges:
+            possible_winner[b]=False
+        winner=-1
+        winner_count=0
+
+        for i in range(0,n):
+            if possible_winner[i]:
+                winner=i
+                winner_count+=1
+        
+        if winner_count==1:
+            return winner
+        return -1
         
