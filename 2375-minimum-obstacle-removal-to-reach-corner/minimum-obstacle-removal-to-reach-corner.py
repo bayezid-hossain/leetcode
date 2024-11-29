24c8442ea34f52b3a1b78484a3d89f85d9c9ec69
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-11-29 11:32:59 +0600

    Time: 1062 ms (83.53%) | Memory: 47.2 MB (26.59%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
@@ -1,36 +1,26 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-        n,m = len(grid), len(grid[0])
-        if n==1 and m==1:
-            return grid[0][0]
-        g = [[] for i in range(m * n)]
-        dx = [1,-1,0,0]
-        dy = [0,0,1,-1]
-        for i in range(n):
-            for j in range(m):
-                for k in range(4):
-                    nx = i + dx[k]
-                    ny = j + dy[k]
-                    if 0<=nx<n and 0<=ny<m:
-                        if grid[nx][ny]==1:
-                            g[i*m + j].append([nx*m + ny, 1])
-                        else:
-                            # print(i,j,i*m + j , len(g))
-                            g[i*m + j].append([nx*m + ny, 0])
-        heap = [[grid[0][0], 0] ]
-        n = len(g)
-        dist = [float('inf')]*n
-        dist[0] = grid[0][0] 
-        # print(g[3])
-        while heap:
-            # print(heap)
-            d,node = heappop(heap)
-            if dist[node]<d:
-                continue 
-            for child,w in g[node]:
-                # print(node,child, dist[child],d,w)
-                if dist[child]>d +w:
-                    dist[child] = d + w 
-                    heappush(heap , [dist[child] , child])
-        # print(dist)
-        return dist[-1]
+        ROWS,COLS=len(grid),len(grid[0])
+        
+        q=deque([(0,0,0)]) #(obstacles,row,col)
+        visited=set({})
+        while q:
+            obstacles,r,c=q.popleft()
+            
+            if r==ROWS-1 and c==COLS-1:
+                return obstacles
+            nei=[[r+1,c],[r,c+1],[r-1,c],[r,c-1]]
+
+            for nr,nc in nei:
+                if nr<0 or nc<0 or (nr,nc) in visited or nr==ROWS or nc==COLS :
+                    continue
+                
+                if grid[nr][nc]:
+                    q.append((obstacles+1,nr,nc))
+                else:
+                    q.appendleft((obstacles,nr,nc))
+
+
+                visited.add((nr,nc))
+            
+            
