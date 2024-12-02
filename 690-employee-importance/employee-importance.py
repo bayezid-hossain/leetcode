40e853ac3907a9b459259e5eec3824b9528aec19
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-12-02 11:38:03 +0600

    Time: 114 ms (61.01%) | Memory: 18.3 MB (6.78%) - LeetSync

---
+++ 690-employee-importance/employee-importance.py
@@ -0,0 +1,27 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        importance=0
+        queue=[id]
+        new_list = sorted(employees, key=lambda x: x.id, reverse=False)
+        # print(employees)
+        for employee in new_list:
+            # print(employee.id)
+            if employee.id in queue:
+                for cur_id in employee.subordinates:
+                    queue.append(cur_id)
+                # print(queue,employee.id)
+                importance+=employee.importance
+                # queue.pop(0)
+
+        # print(queue,importance)
+        print(queue)
+        return importance
