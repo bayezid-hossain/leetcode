b5b526e6808e26f367843312e99ee17c7d7c6cbe
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-12-02 11:13:04 +0600

    Time: 0 ms (100.00%) | Memory: 17.4 MB (6.33%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -3,11 +3,6 @@ class Solution:
         count=0
         length=len(pref)
         for word in words:
-            first,second=0,0
-            length2=len(word)
-            while second<length and first<length2 and word[first]==pref[second]:
-                first+=1
-                second+=1
-            if second==length:
+            if word.startswith(pref):
                 count+=1
         return count
