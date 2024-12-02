c05b06d865237abcff21bd6280df7fba003128ab
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-12-02 11:40:46 +0600

    Time: 114 ms (61.01%) | Memory: 18.5 MB (6.78%) - LeetSync

--- 690-employee-importance/employee-importance.py
+++ 690-employee-importance/employee-importance.py
@@ -9,19 +9,32 @@ class Employee:
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        importance=0
-        queue=set({id})
-        new_list = sorted(employees, key=lambda x: x.id, reverse=False)
-        # print(employees)
-        for employee in new_list:
-            # print(employee.id)
-            if employee.id in queue:
-                for cur_id in employee.subordinates:
-                    queue.add(cur_id)
-                # print(queue,employee.id)
-                importance+=employee.importance
-                # queue.pop(0)
+        # importance=0
+        # queue=set({id})
+        # new_list = sorted(employees, key=lambda x: x.id, reverse=False)
+        # # print(employees)
+        # for employee in new_list:
+        #     # print(employee.id)
+        #     if employee.id in queue:
+        #         for cur_id in employee.subordinates:
+        #             queue.add(cur_id)
+        #         # print(queue,employee.id)
+        #         importance+=employee.importance
+        #         # queue.pop(0)
 
-        # print(queue,importance)
-        # print(queue)
-        return importance
+        # # print(queue,importance)
+        # # print(queue)
+        # return importance
+
+        def dfs(id: int) -> int:
+            e = map[id]
+            sum = e.importance
+            for sub_id in e.subordinates:
+                sum += dfs(sub_id)
+            return sum
+        
+        map = {}
+        for e in employees:
+            map[e.id] = e
+        
+        return dfs(id)
