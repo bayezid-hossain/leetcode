a69e9d3abf27025085ffe2a7402c88dd147497fc
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-12-03 10:51:27 +0600

    Time: 31 ms (98.25%) | Memory: 49 MB (36.87%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -15,19 +15,27 @@ class Solution:
 #         result+=s[spacePointer:]
 #         return result
 
-        result=[]
-        temp=[]
-        spacePointer=0
-        length=len(s)
-        for i in range(length):
-            if spacePointer==len(spaces):
-                temp.append(s[i])
-                continue
-            if i==spaces[spacePointer]:
-                result.append("".join(temp))
-                temp=[s[i]]
-                spacePointer+=1
-            else:
-                temp.append(s[i])
-        result.append("".join(temp))    
+        # result=[]
+        # temp=[]
+        # spacePointer=0
+        # length=len(s)
+        # for i in range(length):
+        #     if spacePointer==len(spaces):
+        #         temp.append(s[i])
+        #         continue
+        #     if i==spaces[spacePointer]:
+        #         result.append("".join(temp))
+        #         temp=[s[i]]
+        #         spacePointer+=1
+        #     else:
+        #         temp.append(s[i])
+        # result.append("".join(temp))    
+        # return " ".join(result)
+        index, result = 0, []
+
+        for space in spaces:
+            result.append(s[index : space])
+            index = space
+        
+        result.append(s[index :])
         return " ".join(result)
