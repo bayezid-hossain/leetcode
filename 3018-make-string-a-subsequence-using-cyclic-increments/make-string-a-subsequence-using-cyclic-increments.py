45261657534a3bf9a50e68f162c289cf426b46fe
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-12-04 11:48:57 +0600

    Time: 66 ms (81.94%) | Memory: 17.5 MB (50.24%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,6 +1,10 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        
+        hashmap = {
+    'a': 'b', 'b': 'c', 'c': 'd', 'd': 'e', 'e': 'f', 'f': 'g', 'g': 'h', 'h': 'i', 'i': 'j', 'j': 'k',
+    'k': 'l', 'l': 'm', 'm': 'n', 'n': 'o', 'o': 'p', 'p': 'q', 'q': 'r', 'r': 's', 's': 't', 't': 'u',
+    'u': 'v', 'v': 'w', 'w': 'x', 'x': 'y', 'y': 'z', 'z': 'a'
+}
         length1=len(str1)
         length2=len(str2)
         if length2>length1:
@@ -10,9 +14,8 @@ class Solution:
         while pointerMain<length1 and pointerSub<length2:
             cur_main=str1[pointerMain]
             next_sub=str2[pointerSub]
-            if cur_main==next_sub or chr((((ord(cur_main)+1)-97)%26)+97)==next_sub:
+            if cur_main==next_sub or hashmap[cur_main]==next_sub:
                 pointerSub+=1
-            
             pointerMain+=1
         return True if pointerSub==length2 else False
 
