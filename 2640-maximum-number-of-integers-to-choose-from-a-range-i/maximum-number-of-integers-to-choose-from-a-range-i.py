9816e9f56b0554834a4069911448975d3ab4cc48
Bayezid Al Hossain <arkantos.tas@gmail.com>
2024-12-06 10:34:45 +0600

    Time: 44 ms (73.68%) | Memory: 18.3 MB (83.39%) - LeetSync

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        arr=[0] *( n+1)
+        
+        for i in banned:
+            if i<=n:
+                arr[i]=1
+                
+        sum=0
+        i=1
+        count=0
+        while sum<=maxSum and i<=n:
+            if arr[i]!=1:
+                sum+=i
+                count+=1
+            i+=1
+        if sum>maxSum:
+            count-=1
+        return count
+            
